234e5029c395428817e47a038d25642e53c54799
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-25 11:37:29 -0500

    google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:travelappg9/pages/home_page.dart';
 import 'package:travelappg9/pages/init_page.dart';
 import 'package:travelappg9/pages/welcome_page.dart';
 import 'package:travelappg9/widgets/welcome_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Poppins',
+        // fontFamily: 'Poppins',
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
       home: InitPage(),
       debugShowCheckedModeBanner: false,
